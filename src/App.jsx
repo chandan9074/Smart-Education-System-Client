6d1a77ba42a81d410c833f96024fedcd417e9257
souraavsen1 <sourav15-11392@diu.edu.bd>
2022-03-03 22:18:50 +0600

    feat: page not found page added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Route, Routes } from "react-router-dom";
 import NavarSection from "./components/NavbarSection";
 import SignIn from "./pages/Authentication/Signin";
 import SignUp from "./pages/Authentication/SignUp";
+import PageNotFound from "./components/NotFound";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Routes>
         <Route path='/signup' element={<SignUp />} />
         <Route path='/signin' element={<SignIn />} />
+        <Route path='*' element={<PageNotFound />} />
       </Routes>
     </div>
   );
---
+++ src/components/NotFound.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NotFound from "../assets/GIF/NotFoundGIF.gif";
+
+const PageNotFound = () => {
+  // this Component is for the wrong routes
+  return (
+    <div>
+      <div className='flex flex-col items-center w-7/12 pb-16 mx-auto my-10 rounded-md'>
+        <img width="70%" src={NotFound} alt='' />
+        <Link
+          to='/'
+          className='color-secendary py-2 px-5 mt-2 rounded-full text-lack font-semibold'
+        >
+          Go back to Home
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default PageNotFound;
